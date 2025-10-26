e47c57eeff40a737b62bb48e2bfd9231eafa8b21
Jorge Niebla Núñez <jorniebla+github@gmail.com>
2025-10-26 22:39:01 +0000

    Prueba con github actions

--- dist/main.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const title = document.createElement("h1");
-title.textContent = "¡Hola desde TypeScript!";
-document.body.appendChild(title);
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,17 @@
+import { PokemonClient, EggGroup, POKEMON_HABITATS } from 'pokenode-ts'; // import the GameClient and the Pokedexes enum
+
 const title = document.createElement("h1");
 title.textContent = "¡Hola desde TypeScript!";
 document.body.appendChild(title);
+
+
+
+(async () => {
+  const api = new PokemonClient(); // create a GameClient
+
+  await api
+    .getPokemonHabitatById(POKEMON_HABITATS.MONTAIN)
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error));
+
+})();
